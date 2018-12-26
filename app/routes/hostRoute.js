371c73c9da39ba94d46a024f4dc84c4bed24e4f4
rokoga <rokoga@gmail.com>
2018-12-26 09:04:24 -0200

    Save

--- app/routes/hostRoute.js
+++ app/routes/hostRoute.js
@@ -1,13 +1,9 @@
-// const express = require('express');
-// const router = express.Router();
-const controller = require('../controllers/hostController')
+const HostController = require("../controllers/HostController");
 
-module.exports = (app) => {
-
-  app.get('/host', controller.get);
-  app.get('/host/:id', controller.getById);
-  app.post('/host', controller.post);
-  app.put('/host/:id', controller.put);
-  app.delete('/host/:id', controller.delete);
-
+module.exports = app => {
+  app.get("/host", (...params) => HostController.get(...params));
+  app.get("/host/:id", (...params) => HostController.get(...params));
+  app.post("/host", (...params) => HostController.post(...params));
+  app.put("/host/:id", (...params) => HostController.put(...params));
+  app.delete("/host/:id", (...params) => HostController.delete(...params));
 };
