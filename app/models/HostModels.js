5f78a097f513ac54ffc53ac32aa861c24d1f5e2d
rokoga <rokoga@gmail.com>
2018-12-26 21:59:57 -0200

    Add Taoka lib

--- app/models/HostModels.js
+++
@@ -1,7 +0,0 @@
-const ModelFactory = require("../resources/ModelFactory");
-
-module.exports = ModelFactory("Host", {
-  name: String,
-  hostgroup: String,
-  so: String
-});
--- app/routes/hostRoute.js
+++
@@ -1,9 +0,0 @@
-const HostController = require("../controllers/HostController");
-
-module.exports = app => {
-  app.get("/host", (...params) => HostController.get(...params));
-  app.get("/host/:id", (...params) => HostController.get(...params));
-  app.post("/host", (...params) => HostController.post(...params));
-  app.put("/host/:id", (...params) => HostController.put(...params));
-  app.delete("/host/:id", (...params) => HostController.delete(...params));
-};
